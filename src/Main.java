6524a8db14928c59ec51339b20731fe93c8c52cd
minho <minho03108@naver.com>
2025-11-24 22:01:55 +0900

    priority 수정

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,17 @@ public class Main {
     public static void main(String[] args) {
         //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
         // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!\n");
+        System.out.println("Hello and welcome!");
 
         List<Process> list = ReadText.getList("temp");
 
         for(Process p : list){
             System.out.println(p.getId());
         }
-        System.out.printf("\n민호");
-        System.out.println("\n해우");
-        System.out.println("\n미지ㅎㅇ");
-        System.out.println("\n도연gd");
+        System.out.println("민호");
+        System.out.println("해우");
+        System.out.println("미지ㅎㅇ");
+        System.out.println("도연gd");
 
         OutText.Print_console();   // 여기서 한 번에 FIFO + PRIORITY 결과 출력
 
