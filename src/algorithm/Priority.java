ffd5f9af0999440295925bdc762f735582568549
minho <minho03108@naver.com>
2025-11-28 01:42:04 +0900

    priority 수정

--- src/algorithm/Priority.java
+++ src/algorithm/Priority.java
@@ -9,59 +9,68 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Priority {
+    //queue2 사이즈 설정
+    static int initSize = CommonUtils.InitSize;
+
     public static Result run() {
         //변수 선언부
-        int totalProcessTime = 0;   // 전체 실행시간
-        int totalWaitTime = 0;      // 전체 대기시간
-        int index = 0;              // 실행큐에서 실행될 PID의 위치 (우선순위에 따라 숫자 결정)
+        int totalProcessTime = 0;                   // 전체 실행시간
+        int totalWaitTime = 0;                      // 전체 대기시간
+        int index = 0;                              // 실행큐에서 실행될 PID의 위치 (우선순위에 따라 숫자 결정)
+        int totalSize = 0;                          // 전체 큐 크기
+        int completedCount = 0;                     // 큐 완료 개수
+        int activeCount = 0;                        // 실행큐에 null이 아닌 개수
 
-        Process longProcess = new Process();    // 프로세스 최장 시간
-        Process shortProcess = new Process();   // 프로세스 최단 시간
+        Process longProcess = new Process();        // 프로세스 최장 시간
+        Process shortProcess = new Process();       // 프로세스 최단 시간
 
+        // 데이터 로드 - 추후 경로 받기 필요
+        List<Process> wait_queue = ReadText.getProcessList("");
 
-        List<Process> wait_queue = ReadText.getProcessList(""); //파일에서 대기큐 가져오기
+        totalSize = wait_queue.size();                                      // 전체 큐 개수 저장
 
-        int totalSize = wait_queue.size();  // 대기큐 크기
+        List<Process> run_queue = new ArrayList<>(initSize);                // 실행큐 선언
 
-        List<Process> run_queue = new ArrayList<>(10); // 실행큐 선언
-
-        System.out.println("초기 대기큐 -> 실행큐 진입");
-        while (!wait_queue.isEmpty() && run_queue.size() < 10) {    //실행큐 10개 채울 때까지 반복
+        while (!wait_queue.isEmpty() && run_queue.size() < initSize) {      // 실행큐 10개 채울 때까지 반복
             Process process = wait_queue.remove(0);
             run_queue.add(process);
         }
 
-        while (!run_queue.isEmpty()) {
+        //처리된 개수가 전체 개수와 같아질 때까지 반복
+        while (completedCount < totalSize) {
 
-            /*
-            //<!--보여주기용
-            for (int j = 0; j < run_queue.size(); j++) {
-                if (j != 0) {
-                    System.out.print(" - ");
-                }
-                System.out.print(run_queue.get(j).getId());
+            index = bestProcess(run_queue);                                 // 우선순위 기준으로 인덱스 값 가져오기
+            Process process = run_queue.get(index);                         // 우선순위 기준 인덱스위치의 process가져오기
+
+            if (process == null) {
+                continue;
             }
-            System.out.println();
-            //-->보여주기용
-            */
 
-            index = bestProcess(run_queue);             // 우선순위 기준으로 인덱스 값 가져오기
-            Process process = run_queue.remove(index);  // 우선순위 기준 인덱스위치의 process가져오기
-
-            //System.out.println(process.getId() + " - 실행" + "priority : " + process.getPriority());
+            activeCount = 0;
+            for (Process p : run_queue) {
+                if (p != null) activeCount++;
+            }
 
             totalProcessTime += process.getProcessTime();                   // 실행시간
-            totalWaitTime += process.getProcessTime() * run_queue.size();   // 대기시간
+            totalWaitTime += process.getProcessTime() * (activeCount-1);      // 대기시간
             longProcess = CommonUtils.getLongProcess(process);              // 최장시간
             shortProcess = CommonUtils.getShortProcess(process);            // 최단시간
 
             if (!wait_queue.isEmpty()) {
-                Process newProcess = wait_queue.remove(0);              // 대기큐의 제일 앞의 프로세스를
+                Process newProcess = wait_queue.remove(0);            // 대기큐의 제일 앞의 프로세스를 가져옴
                 if (index <= run_queue.size()) {
-                    run_queue.add(index, newProcess);
+                    run_queue.set(index, newProcess);
                 }
             }
+            // 대기큐가 비었으면 현재 자리를 null로 만들어서 "빈 자리" 표시
+            else{
+                run_queue.set(index, null);
+            }
+
+            completedCount++;
         }
+
+        // OutText에 보낼 Data Setting
         Result result = new Result();
         result.name = "Priority";
         result.totalProcessTime = totalProcessTime;
@@ -82,10 +91,24 @@ public class Priority {
     // run_queue 에서 최고 우선순위 프로세스 찾기 (찾아서 해당 Index값 반환)
     public static int bestProcess(List<Process> run_queue) {
         int bestindex = 0;
-        Process best = run_queue.get(0);   // 첫번째 프로세스를 최고 우선순위라고 가정, 뒤의 프로세스와 비교
+        Process best = null;
+        
+        // 초기 비교 값 설정
+        for (int i = 0; i < run_queue.size(); i++) {
+            best = run_queue.get(i);
+            bestindex = i;
+            
+            if(best != null) //best 값이 null일 경우 for문 반복 아니면 break
+                break;
+        }
+        
+        for (int i = bestindex + 1; i < run_queue.size(); i++) {    // 반복하며 비교
+            Process cur = run_queue.get(i);             // 비교할 프로세스를 cur 변수에 담음
 
-        for (int i = 1; i < run_queue.size(); i++) { // 반복하며 비교
-            Process cur = run_queue.get(i); // 비교할 프로세스를 cur 변수에 담음
+            if (cur == null)                            // null값이면 무시하고 다음 공간 비교
+                continue;
+            else if(best == null)                       // best가 null일경우 break (그럴 일 없지만 예외처리)
+                break;
 
             // 1) 우선순위가 더 작으면 더 높은 우선순위
             if (cur.priority < best.priority) {
